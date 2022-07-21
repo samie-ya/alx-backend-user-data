16267212b2597c244e2e8a07bc615cc5a8e26450
Samie <samrasolomon40@gmail.com>
2022-07-21 21:13:24 +0000

    Removed field list maker

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -46,7 +46,7 @@ def get_logger() -> logging.Logger:
     logger = logging.Logger('user_data')
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    formatter = RedactingFormatter(list(PII_FIELDS))
+    #formatter = RedactingFormatter(list(PII_FIELDS))
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.propagate = False
