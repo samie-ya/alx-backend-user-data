211a06cfcd0c8ea1a12bbfee29619cfbc71ccecb
Samie <samrasolomon40@gmail.com>
2022-07-22 00:01:00 +0000

    Added validation way for password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(byte, salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """This function will check password against hashed"""
+    passer = password.encode('UTF-8')
+    return bcrypt.checkpw(passer, hashed_password)
