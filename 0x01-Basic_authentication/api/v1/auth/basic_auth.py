b2bddfb9739ddea70953049bedfe08238586b194
Samie <samrasolomon40@gmail.com>
2022-07-25 21:17:35 +0000

    Added credential separator

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,16 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception as e:
             pass
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """This function will extract user credential from base64"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        credential = decoded_base64_authorization_header.split(':')
+        return (credential[0], credential[1])
