8959691344ea455582fdd0faeeb4a237b510527c
Samie <samrasolomon40@gmail.com>
2022-07-25 21:08:55 +0000

    Added a decoder

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """This script will contain the child of Auth"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,18 @@ class BasicAuth(Auth):
         if authorization_header.split(' ')[0] != "Basic":
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """This function will decode the base64 encoded"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            byte_64 = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(byte_64)
+            return decoded.decode('utf-8')
+        except Exception as e:
+            pass
