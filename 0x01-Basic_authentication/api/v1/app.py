9b9f03f70287891fe90c40558b93060a49c2a4c9
Samie <samrasolomon40@gmail.com>
2022-07-26 15:10:02 +0000

    Added a child class BasicAuth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 env = getenv('AUTH_TYPE')
-if env:
+if env == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+if env == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
