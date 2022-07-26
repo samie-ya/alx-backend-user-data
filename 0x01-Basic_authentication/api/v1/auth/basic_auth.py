a67c19b51a0bb117d2c37b483050b69c282e5dab
Samie <samrasolomon40@gmail.com>
2022-07-26 15:11:27 +0000

    Readded extract and decode functions

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,5 +8,34 @@ from typing import TypeVar
 
 
 class BasicAuth(Auth):
-    """This class is empty for now"""
-    pass
+    """This class will create BasicAuth"""
+
+    def __init__(self):
+        """This function will initialize the class"""
+        super(BasicAuth, self).__init__()
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """This function returns the base64 pasrt of Authorization"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header.split(' ')[0] != "Basic":
+            return None
+        return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """This function will decode the base64 encoded"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            byte_64 = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(byte_64)
+            return decoded.decode('utf-8')
+        except Exception as e:
+            pass
