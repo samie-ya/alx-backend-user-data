cf15aee36d14630fcccb00eb3ef480b5e8b482b7
Samie <samrasolomon40@gmail.com>
2022-07-26 09:19:48 +0000

    Added credential finder

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -50,3 +52,19 @@ class BasicAuth(Auth):
             return (None, None)
         credential = decoded_base64_authorization_header.split(':')
         return (credential[0], credential[1])
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """This function will return user instance"""
+        if (user_email is None) or (type(user_email) != str):
+            return None
+        if (user_pwd is None) or (type(user_pwd) != str):
+            return None
+        users = User.search()
+        if len(users) != 0:
+            for user in users:
+                if user.email == user_email:
+                    if user.is_valid_password(user_pwd):
+                        return user
+            return None
+        return None
