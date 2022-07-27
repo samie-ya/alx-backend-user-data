72b22bd0bceeea0111df7029f0a9cdc049ab38d9
Samie <samrasolomon40@gmail.com>
2022-07-27 18:22:44 +0000

    Added proper conditional to befor request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,7 +57,7 @@ def before_request():
         pass
     else:
         if (auth.authorization_header(request) is None) and\
-           (auth.session_cookie(request)):
+           (auth.session_cookie(request) is None):
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
