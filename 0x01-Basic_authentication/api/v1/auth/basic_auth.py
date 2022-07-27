c6d16f4bd544812c38e793c232ad3440504452e4
Samie <samrasolomon40@gmail.com>
2022-07-27 08:47:43 +0000

    Added return value if no email

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,7 +68,8 @@ class BasicAuth(Auth):
                 if user.email == user_email:
                     if user.is_valid_password(user_pwd):
                         return user
-            return None
+                    return None
+                return None
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
