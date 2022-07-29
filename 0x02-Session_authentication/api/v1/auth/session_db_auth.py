3e302471cb1e8f2f67cf6b91e44cdc646026bc83
Samie <samrasolomon40@gmail.com>
2022-07-29 19:18:44 +0000

    Redo on session_db_auth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+import uuid
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -14,20 +15,33 @@ class SessionDBAuth(SessionExpAuth):
 
     def create_session(self, user_id=None):
         """ This function will create a session"""
-        session_id = super.()create_session()
-        user_sess = UserSession(user_id=user_id, session_id=session_id)
-        return session_id
+        if (user_id is None) or (type(user_id) != str):
+            return None
+        sess_id = super().create_session(user_id)
+        values = {'user_id': user_id, 'session_id': sess_id}
+        user_sess = UserSession(**values)
+        user_sess.save()
+        return sess_id
 
     def user_id_for_session_id(self, session_id=None):
         """This function will find user_id"""
-        user_id = self.user_id_by_session_id[session_id]['user_id']
-        return user_id
+        if (session_id is None) or (type(session_id) != str):
+            return None
+        users = UserSession.search()
+        ls = [user for user in users if user.session_id == session_id]
+        try:
+            user = ls[0]
+            return user.user_id
+        except IndexError as e:
+            pass
 
     def destroy_session(self, request=None):
         """This function will delete a session"""
         cookie_value = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(session_id)
         users = UserSession.search()
-        ls = [user for user in users if user_id == user.id]
-        user_sess = ls[0]
-        user_sess.remove()
+        ls = [user for user in users if user.session_id == cookie_value]
+        try:
+            user = ls[0]
+            user.remove()
+        except IndexError as e:
+            pass
