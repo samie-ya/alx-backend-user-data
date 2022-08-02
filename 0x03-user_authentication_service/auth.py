53c9e4e86f364818d7245e49bb3f3d030808a734
Samie <samrasolomon40@gmail.com>
2022-08-02 10:20:22 +0000

    Added Auth classes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """This function will create a byte from str"""
 import bcrypt
+from db import DB
+from typing import TypeVar
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +11,24 @@ def _hash_password(password: str) -> bytes:
     encrypt = password.encode('UTF-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(encrypt, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """This function will register a user"""
+        session = self._db._session
+        user = session.query(User).filter_by(email=email).first()
+        if user:
+            raise ValueError('User {} already exists'.format(email))
+        else:
+            hashed = _hash_password(password)
+            new_user = User(email=email, hashed_password=hashed)
+            session.add(new_user)
+            session.commit()
+            return new_user
