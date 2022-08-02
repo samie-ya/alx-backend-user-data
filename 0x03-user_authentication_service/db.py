ccbcaea20399b7ba7522eed1287065560b3df96e
Samie <samrasolomon40@gmail.com>
2022-08-02 09:30:06 +0000

    Removed future functions and added right parameter

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,13 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-from typing import TypeVar
-from user import Base, User
+
+from user import Base
 
 
 class DB:
@@ -32,23 +30,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hash_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """This function will add user to database"""
-        new_user = User(email=email, hashed_password=hash_password)
+        new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
-    def find_user_by(self, **kwargs: dict) -> TypeVar('User'):
-        """This function will use the keyword to query table user"""
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user:
-            return user
-        raise NoResultFound
-
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
-        """This function will update a given user_id"""
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            setattr(user, key, value)
-        self._session.commit()
