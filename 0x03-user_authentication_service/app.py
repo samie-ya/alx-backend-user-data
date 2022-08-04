f8d3a6a03739762f1e57e2fd9a2c5ec0fd0b8f8e
Samie <samrasolomon40@gmail.com>
2022-08-04 20:43:17 +0000

    Made correction on typo

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,7 +48,7 @@ def logout():
     user = AUTH.get_user_from_session_id(cookie_value)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect(url_for('basic')i)
+        return redirect(url_for('basic'))
     else:
         abort(403)
 
