5c86fa26cbc1dd93323e59fe1016a3e75d29766d
Samie <samrasolomon40@gmail.com>
2022-08-04 16:59:22 +0000

    Only added type annonations to parameters

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str):
         """This function will add user to database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
